6260af1246728680b68d05856f31c0ad43d39252
sugyan <sugi1982@gmail.com>
2013-03-29 20:31:00 +0900

    update

--- lib/CasualApp/Web/Dispatcher.pm
+++ lib/CasualApp/Web/Dispatcher.pm
@@ -6,13 +6,28 @@ use Amon2::Web::Dispatcher::Lite;
 
 any '/' => sub {
     my ($c) = @_;
-    return $c->render('index.tt');
+    my $entries = $c->dbh->selectall_arrayref(
+        'SELECT * FROM entry',
+        { Slice => {} },
+    );
+    return $c->render('index.tt', { entries => $entries });
 };
 
 get '/account/logout' => sub {
     my ($c) = @_;
     $c->session->expire();
     return $c->redirect('/');
+};
+
+post '/post' => sub {
+    my ($c) = @_;
+    my $body = $c->req->param('body');
+    $c->dbh->do(
+        'INSERT INTO entry (user_name, body) values (?, ?)',
+        undef,
+        $c->session->get('name'), $body,
+    );
+    $c->redirect('/');
 };
 
 1;
--- sql/sqlite.sql
+++ sql/sqlite.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS sessions (
     id           CHAR(72) PRIMARY KEY,
     session_data TEXT
 );
+
+CREATE TABLE IF NOT EXISTS entry (
+    id           integer PRIMARY KEY,
+    user_name varchar(255) not null,
+    body text not null
+);
